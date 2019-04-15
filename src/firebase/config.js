179a71ee0144da74cccc0a71e8322bbe9140dd8d
Divyansh Tiwari <divyansh@smilebots.com>
2019-04-15 11:01:06 +0530

    init 2

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,5 @@
 const config = {
-  apiKey: "AIzaSyClVS1x-iJsp44XW6fwCmdYhneNCTS460A",
-  authDomain: "yourwheeler-app.firebaseapp.com",
-  databaseURL: "https://yourwheeler-app.firebaseio.com",
-  projectId: "yourwheeler-app",
-  storageBucket: "yourwheeler-app.appspot.com",
-  messagingSenderId: "229848858487"
+  //enter your firebase configuration settings here
 };
 
 export default config;
