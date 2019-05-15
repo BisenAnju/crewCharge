0bea9325a35b566330da545ec806701c68bdb02b
Divyansh Tiwari <divyansh@smilebots.com>
2019-05-15 10:41:37 +0530

    init again

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import withMUI from "./hoc/withMUI";
+import withUser from "./hoc/withUser";
+import withAuthentication from "./hoc/withAuthentication";
+import NewVehicle from "./containers/NewVehicle";
+
+const App = props => (
+  <div>
+    <Router>
+      <div>
+        <Route path="/" render={props => <NewVehicle {...props} />} />
+      </div>
+    </Router>
+  </div>
+);
+
+export default withAuthentication(withMUI(withUser(App)));
