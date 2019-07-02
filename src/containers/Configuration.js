9a69e5bb6e99bf920fa3fd11bdc6791e7a36209d
Anju Bisen <bisen4267@gmail.com>
2019-07-02 12:37:48 +0530

    Added Dropzone For image uploading

--- src/containers/Configuration.js
+++ src/containers/Configuration.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  withRouter,
-  Switch
-} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import Configuration from "../components/Configuration";
 import withFirebase from "../hoc/withFirebase";
 
@@ -14,41 +9,28 @@ class ConfigurationContainer extends React.Component {
     this.state = {};
   }
 
-  addPurpose = (field, collection, purpose) => {
-    this.props.db
-      .collection(collection)
-      .add({
-        [field]: purpose
-          .replace(" ", "")
-          .replace("/", "")
-          .replace("-", "")
-          .toLowerCase(),
-        displayName: purpose.charAt(0).toUpperCase() + purpose.slice(1)
-      })
-      .then
-      // this.props.history.goBack()
-      ()
-      .catch(err => {
-        console.log("Error getting documents", err);
-      });
+  addPurpose = (field, collection, data) => {
+    console.log(data);
+    // this.props.db
+    //   .collection(collection)
+    //   .add({
+    //     [field]: data.purpose
+    //       .replace(" ", "")
+    //       .replace("/", "")
+    //       .replace("-", "")
+    //       .toLowerCase(),
+    //     displayName:
+    //       data.purpose.charAt(0).toUpperCase() + data.purpose.slice(1)
+    //     //  icon:
+    //   })
+    //   .then //     this.props.history.goBack()
+    //   .catch(err => {
+    //     console.log("Error getting documents", err);
+    //   });
   };
 
   render() {
-    return (
-      <div>
-        <Router>
-          <Switch>
-            <Route
-              exact
-              path={"/configuration"}
-              render={props => (
-                <Configuration {...props} addPurpose={this.addPurpose} />
-              )}
-            />
-          </Switch>
-        </Router>
-      </div>
-    );
+    return <Configuration addPurpose={this.addPurpose} />;
   }
 }
 export default withFirebase(withRouter(ConfigurationContainer));
