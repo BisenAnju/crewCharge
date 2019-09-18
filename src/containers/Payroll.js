b9d11441cd49a3054ff0e24aeb24fa48e4391656
Anju Bisen <bisen4267@gmail.com>
2019-09-18 10:32:04 +0530

    payroll container

--- src/containers/Payroll.js
+++ src/containers/Payroll.js
@@ -6,13 +6,37 @@ import withFirebase from "../hoc/withFirebase";
 class PayrollContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userData: [],
+      isLoading: true
+    };
   }
-
+  componentWillMount() {
+    this.props.db
+      .collection("users")
+      // .orderBy("displayName")
+      .onSnapshot(
+        snapshot => {
+          const userData = [];
+          snapshot.forEach(doc => {
+            if (doc.exists) {
+              userData.push(doc.data());
+            }
+          });
+          this.setState({
+            isLoading: false,
+            userData
+          });
+        },
+        err => {
+          console.log(`Encountered error: ${err}`);
+        }
+      );
+  }
   render() {
     return (
       <div>
-        <Payroll />
+        <Payroll userData={this.state.userData} />
       </div>
     );
   }
